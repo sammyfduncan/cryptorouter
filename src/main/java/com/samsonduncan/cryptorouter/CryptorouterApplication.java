5bdaad01863718ce6c1de330444b95a9a96b9238
sammyfduncan <SD192837465@hotmail.co.uk>
2025-09-21 17:26:18 +0100

    Initial commit

---
+++ src/main/java/com/samsonduncan/cryptorouter/CryptorouterApplication.java
@@ -0,0 +1,13 @@
+package com.samsonduncan.cryptorouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptorouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptorouterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samsonduncan/cryptorouter/CryptorouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsonduncan.cryptorouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptorouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
